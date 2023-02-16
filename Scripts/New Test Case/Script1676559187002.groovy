16c0fdf254d3f09b13805da07eafe7b67f823895
Abeer Abu Zayd <abeer.abuzayd@inspirejo.com>
2023-02-16 18:08:45 +0300

    wer

--- Scripts/New Test Case/Script1676559187002.groovy
+++ Scripts/New Test Case/Script1676559187002.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('https://erp-qa.inspirejo.com/Tasks')
+WebUI.openBrowser('https://www.zara.com/jo/')
 
